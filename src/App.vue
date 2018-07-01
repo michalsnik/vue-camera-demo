f0570422a5cef35b1d5b90d6c05a8cde5440de79
Michał Sajnóg <msajnog93@gmail.com>
2018-07-01 02:20:03 +0200

    Initial camera implementation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-camera
+      ref="camera"
+      :capture-on-click="true"
+      class="camera"
+      @capture="onCapture"
+    />
+    <img
+      :src="img"
+      class="preview"
+    >
+    <button
+      class="trigger"
+      @click="$refs.camera.capture()"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import VueCamera from "./components/VueCamera.vue";
 
 export default {
-  name: "app",
+  name: "App",
   components: {
-    HelloWorld
+    VueCamera
+  },
+  data() {
+    return {
+      img: ""
+    };
+  },
+  methods: {
+    onCapture(img) {
+      this.img = img;
+    }
   }
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style>
+body {
+  margin: 0;
+}
+</style>
+
+<style lang="scss" scoped>
+.camera {
+  width: 100vw;
+  height: 100vh;
+}
+
+.preview {
+  position: fixed;
+  left: 15px;
+  bottom: 15px;
+  z-index: 10;
+  width: 80px;
+  height: 60px;
+  border-radius: 2px;
+  object-fit: cover;
+}
+
+.trigger {
+  position: fixed;
+  bottom: 20px;
+  left: 0;
+  right: 0;
+  z-index: 10;
+  margin: auto;
+  width: 50px;
+  height: 50px;
+  outline: none;
+  border: 3px solid #fff;
+  background: none;
+  border-radius: 50%;
 }
 </style>
