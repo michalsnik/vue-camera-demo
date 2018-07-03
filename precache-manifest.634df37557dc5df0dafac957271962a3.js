6e1e97b761f37be4776cf5d195e9ef943a62c602
Michał Sajnóg <msajnog93@gmail.com>
2018-07-04 01:09:59 +0200

    Updates

---
+++ precache-manifest.634df37557dc5df0dafac957271962a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45f0748c3b9845b09eb2",
+    "url": "/vue-camera-demo/js/chunk-vendors.45f0748c.js"
+  },
+  {
+    "revision": "a3c1230373f6dae0a688",
+    "url": "/vue-camera-demo/js/app.a3c12303.js"
+  },
+  {
+    "revision": "923a0ae41b93e02e8bf227d0e48068d6",
+    "url": "/vue-camera-demo/index.html"
+  },
+  {
+    "revision": "45f0748c3b9845b09eb2",
+    "url": "/vue-camera-demo/css/chunk-vendors.7cc27502.css"
+  },
+  {
+    "revision": "a3c1230373f6dae0a688",
+    "url": "/vue-camera-demo/css/app.d91c81ca.css"
+  }
+];
